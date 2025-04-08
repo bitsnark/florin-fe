130c18f109a9194934343651745477215f853aca
Nahuel Jaume <nahueljaume@gmail.com>
2025-04-08 12:00:08 -0300

    absolute paths and readme init

--- src/routes/history.tsx
+++ src/routes/history.tsx
@@ -1,9 +1,6 @@
+import { History } from '@/pages/History'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/history')({
-  component: RouteComponent,
+  component: History,
 })
-
-function RouteComponent() {
-  return <div>Hello "/history"!</div>
-}
