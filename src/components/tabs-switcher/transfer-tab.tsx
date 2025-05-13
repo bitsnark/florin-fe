d465dcf96c41fafd95354330f775c3c86d7cf8e8
Nahuel Jaume <nahueljaume@gmail.com>
2025-05-13 11:15:20 -0300

    tab switcher refactor improves

--- src/components/tabs-switcher/transfer-tab.tsx
+++ src/components/tabs-switcher/transfer-tab.tsx
@@ -1,58 +1,111 @@
+import { useState } from 'react';
 import { TransferForm } from './transfer-form';
 import { FeeCard } from './fee-card';
 import { TermsSection } from './terms-section';
 import { ConnectButton } from './connect-button';
-import { Address } from 'viem';
 import { Button } from '../ui/button';
 import { isValidBitcoinAddress } from '@/lib/utils';
+import { Network, Currency } from './types';
+import { useChainId } from 'wagmi';
+import { Address, parseEther } from 'viem';
+import { useExchange } from '@/hooks/useExchange';
+import { Position, Reservation } from '@/types';
+import { useMaxMinBtc } from '@/hooks/queries/useMaxMinBtc';
+import { useBitSnarkBalance } from '@/hooks/useBitSnarkBalance';
+import { useAccount } from 'wagmi';
 
 interface TransferTabProps {
-  fromNetwork: 'bitcoin' | 'ethereum';
-  toNetwork: 'bitcoin' | 'ethereum';
-  fromCurrency: 'btc' | 'eth' | 'xbtc';
-  toCurrency: 'btc' | 'eth' | 'xbtc';
-  fromAmount: string;
-  toAmount: string;
-  xbtcAmount: string;
-  isAnimating: boolean;
-  isWalletConnected: boolean;
-  ethWalletAddress?: Address;
-  bitcoinAddress?: string;
-  termsAccepted: boolean;
-  handleSwitchNetworks: () => void;
-  handleFromAmountChange: (value: string) => void;
-  handleToAmountChange: (value: string) => void;
-  setBitcoinAddress: (value: string | undefined) => void;
-  setTermsAccepted: (value: boolean) => void;
-  handleBridgeFunds: () => void;
-  loading: boolean;
-  maxBtc: number;
-  minBtc: number;
+  onTransactionCreated: (type: 'position' | 'reservation', id: string) => void;
 }
 
-export function TransferTab({
-  fromNetwork,
-  toNetwork,
-  fromCurrency,
-  toCurrency,
-  fromAmount,
-  toAmount,
-  xbtcAmount,
-  isAnimating,
-  isWalletConnected,
-  ethWalletAddress,
-  bitcoinAddress,
-  termsAccepted,
-  handleSwitchNetworks,
-  handleFromAmountChange,
-  handleToAmountChange,
-  setBitcoinAddress,
-  setTermsAccepted,
-  handleBridgeFunds,
-  loading,
-  maxBtc,
-  minBtc,
-}: TransferTabProps) {
+export function TransferTab({ onTransactionCreated }: TransferTabProps) {
+  const { address } = useAccount();
+  const chainId = useChainId();
+  const [fromNetwork, setFromNetwork] = useState<Network>('bitcoin');
+  const [toNetwork, setToNetwork] = useState<Network>('ethereum');
+  const [fromCurrency, setFromCurrency] = useState<Currency>(
+    fromNetwork === 'bitcoin' ? 'btc' : 'eth'
+  );
+  const [toCurrency, setToCurrency] = useState<Currency>(
+    toNetwork === 'bitcoin' ? 'btc' : 'xbtc'
+  );
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [fromAmount, setFromAmount] = useState('0.012');
+  const [toAmount, setToAmount] = useState('0.012');
+  const [termsAccepted, setTermsAccepted] = useState(false);
+  const [bitcoinAddress, setBitcoinAddress] = useState<string | undefined>(undefined);
+  
+  const { openPosition, reservePosition, loading } = useExchange();
+  const { data } = useMaxMinBtc();
+  const maxBtc = data?.maxAmount || 0;
+  const minBtc = data?.minAmount || 0;
+  const { balance: xbtcAmount } = useBitSnarkBalance();
+  const isWalletConnected = !!address;
+
+  const handleSwitchNetworks = () => {
+    if (isAnimating) return;
+
+    setIsAnimating(true);
+
+    setTimeout(() => {
+      setFromNetwork(toNetwork);
+      setToNetwork(fromNetwork);
+      setFromCurrency(toCurrency);
+      setToCurrency(fromCurrency);
+
+      const tempAmount = fromAmount;
+      setFromAmount(toAmount);
+      setToAmount(tempAmount);
+
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 300);
+    }, 300);
+  };
+
+  const handleFromAmountChange = (value: string) => {
+    setFromAmount(value);
+    setToAmount(value);
+  };
+
+  const handleToAmountChange = (value: string) => {
+    setToAmount(value);
+    if (fromCurrency === 'xbtc' && toCurrency === 'btc') {
+      setFromAmount(value);
+    }
+  };
+
+  const handleBridgeFunds = async () => {
+    const normalizedAmount = fromAmount.replace(',', '.');
+
+    let transaction: Position | Reservation | undefined;
+    if (fromNetwork === 'bitcoin') {
+      transaction = await reservePosition({
+        tokenAmount: parseEther(normalizedAmount),
+        evmReceivingAddress: address!,
+        chainId,
+        owner: address!,
+      });
+    } else {
+      transaction = await openPosition({
+        tokenAmount: parseEther(normalizedAmount),
+        exchangeRate: 1,
+        bitcoinAddresses: bitcoinAddress! as Address,
+        deadline: Math.floor(Date.now() / 1000) + 3600,
+        owner: address!,
+        chainId,
+      });
+    }
+    if (transaction) {
+      onTransactionCreated(
+        fromNetwork === 'ethereum' ? 'position' : 'reservation',
+        fromNetwork === 'ethereum'
+          ? (transaction as Position)?.positionId
+          : (transaction as Reservation)?.reservationId
+      );
+    }
+  };
+
   // TODO: REFACTOR, validate form using zod
   const isBitcoinAddressValid =
     fromNetwork === 'ethereum' && toCurrency === 'btc'
@@ -87,10 +140,10 @@ export function TransferTab({
         toCurrency={toCurrency}
         fromAmount={fromAmount}
         toAmount={toAmount}
-        xbtcAmount={xbtcAmount}
+        xbtcAmount={xbtcAmount.toString()}
         isAnimating={isAnimating}
         isWalletConnected={isWalletConnected}
-        ethWalletAddress={ethWalletAddress}
+        ethWalletAddress={address}
         bitcoinAddress={bitcoinAddress}
         bitcoinAddressValid={isBitcoinAddressValid}
         handleSwitchNetworks={handleSwitchNetworks}
