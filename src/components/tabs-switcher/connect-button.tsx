e527da399134b8ef1d3c64d694e7e1136896c961
Nahuel Jaume <nahueljaume@gmail.com>
2025-05-22 16:34:57 -0300

    unsupported network feature

--- src/components/tabs-switcher/connect-button.tsx
+++ src/components/tabs-switcher/connect-button.tsx
@@ -4,21 +4,25 @@ import { useWalletDialog } from '@/hooks/useWalletDialog';
 interface ConnectButtonProps {
   isWalletConnected: boolean;
   isAnimating: boolean;
+  isSupported: boolean;
 }
 
 export function ConnectButton({
   isWalletConnected,
   isAnimating,
+  isSupported,
 }: ConnectButtonProps) {
   const { openDialog } = useWalletDialog();
-  return isWalletConnected ? null : (
+  const label = isSupported ? 'Connect wallet' : 'Unsupported network';
+  return isWalletConnected && isSupported ? null : (
     <Button
       onClick={openDialog}
       isAnimating={isAnimating}
       variant="orange"
       size="custom"
+      disabled={!isSupported}
     >
-      {'Connect wallet'}
+      {label}
     </Button>
   );
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,9 @@
 import { ConnectorsListDialog } from '@/components/evm-wallet-connector/connectors-list-dialog';
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
-//import { FlorinApiService } from '@/services/Api';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { useEffect } from 'react';
 
 const Layout = () => {
-
-  useEffect(() => {
-    /* FlorinApiService.clearAllMockData();
-    FlorinApiService.seed(); */
-  },[])
-  
   return  (
     <div className="w-full min-h-screen flex flex-col">
       <Header />
